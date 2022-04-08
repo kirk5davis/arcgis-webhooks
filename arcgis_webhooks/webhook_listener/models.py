bc42d3f11f51a3ac003748ea534d6995af505d52
Kirk Davis <kirk.davis@watech.wa.gov>
2022-04-08 10:18:59 -0700

    added Job and JobKind model definitions

--- arcgis_webhooks/webhook_listener/models.py
+++ arcgis_webhooks/webhook_listener/models.py
@@ -1,5 +1,13 @@
+from datetime import datetime
+from doctest import DocTestFinder
 import uuid
+import types
+import logging
+from xml.etree.ElementInclude import default_loader
 from django.db import models
+from django.utils.timezone import make_aware
+from django.utils.module_loading import import_string
+
 
 
 class ArcGISWebhookMessage(models.Model):
@@ -17,3 +25,67 @@ class ArcGISWebhookMessage(models.Model):
         ]
         verbose_name = "ArcGIS Webhook"
         verbose_name_plural = "ArcGIS Webhooks"
+
+
+class JobKind(models.Model):
+    id = models.AutoField(primary_key=True)
+    dotted_path = models.TextField(null=False, blank=False, unique=True)
+    created = models.DateTimeField(auto_now_add=True, null=False, blank=False)
+    last_modified = models.DateTimeField(auto_now_add=True, null=False, blank=False)
+
+    def __str__(self):
+        return self.dotted_path
+    
+    @staticmethod
+    def from_func(func):
+        if isinstance(func, types.FunctionType):
+            dotted_path = f"{func.__module__}.{func.__name__}"
+            ret, _ = JobKind.objects.get_or_create(dotted_path=dotted_path)
+            return ret
+        else:
+            raise TypeError
+
+    def run(self, job):
+        try:
+            func = import_string(self.dotted_path)
+            return func(job)
+        except ImportError:
+            logging.error(f"Could not resolve job dotted_path: {self.dotted_path}")
+            raise ImportError
+        
+
+class Job(models.Model):
+    id = models.AutoField(primary_key=True)
+    kind = models.ForeignKey(JobKind, null=True, on_delete=models.SET_NULL)
+    created = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=True, null=False, blank=False)
+    periodic = models.BooleanField(default=False, null=False, blank=False)
+    failed = models.BooleanField(default=None, null=True, blank=True)
+    last_run = models.DateTimeField(default=None, null=True, blank=True)
+    logs = models.TextField(null=True, blank=True)
+    data = models.JSONField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.kind} {self.data}"
+    
+    def run(self):
+        if not self.kind_id:
+            return
+        self.last_run = make_aware(datetime.now())
+        try:
+            res = self.kind.run(self)
+            if res and not self.periodic:
+                self.active = False
+            if isinstance(res, str):
+                if self.logs is None:
+                    self.logs = ""
+                self.logs += res
+            self.failed = False
+            self.save(update_fields=["last_run", "failed", "active", "logs"])
+        except Exception as exc:
+            if self.logs is None:
+                self.logs = ""
+            self.logs += str(exc)
+            self.failed = True
+            self.save(update_fields=["last_run", "failed", "logs"])
+        return
